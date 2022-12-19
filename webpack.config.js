b5b54e4bccc39aa94754c5f77eac8c20f73e4058
padosum <makalu171@gmail.com>
2022-12-19 23:53:51 +0900

    📦 webpack-dev-server, webpack-dev-middleware, webpack-hot-middleware 설치 및 설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,21 @@
-const path = require("path");
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 3000,
+    host: `localhost`,
+  },
+  devtool: 'eval-source-map',
   entry: {
-    main: "./src/app.js",
+    main: ['webpack-hot-middleware/client?reload=true&timeout=1000', './src/client/App.js'],
   },
   output: {
-    path: path.resolve("./dist"),
-    filename: "[name].js",
+    path: path.resolve('./dist'),
+    filename: '[name].js',
   },
   module: {
     rules: [
@@ -14,9 +23,10 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
         },
       },
     ],
   },
+  plugins: [new HtmlWebpackPlugin({title: 'Development'}), new webpack.HotModuleReplacementPlugin()],
 };
