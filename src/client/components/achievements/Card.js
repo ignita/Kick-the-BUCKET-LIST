babd6d3593d8d5f653d395c6e6fe48e7b87a50cf
padosum <makalu171@gmail.com>
2023-01-09 15:34:02 +0900

    ✨ 하위 항목의 달성률을 표시하기 위한 progress-bar 추가
    - 하위 항목  completed 비율을 구해서 width 값으로 설정.

--- src/client/components/achievements/Card.js
+++ src/client/components/achievements/Card.js
@@ -20,16 +20,34 @@ export default class AchievementsCard {
 
   renderSubAchievements() {
     return this.state.subAchievements
-      .map(({ title, description, completed, completedDate, isFailure }) => {
+      .map(({ title, description, completed, completedDate, completedYear, isFailure }) => {
         const icon = this.renderIcon({ isFailure, completed });
+        const completedDateFormat = completedDate ? completedDate : completedYear ? completedYear : '';
         return `<div class="sub-achievement-title">
           ${icon} ${title}</span>
         </div>
-        <div class="sub-achievement-completed">${completed ? completedDate : ''}</div>
+        <div class="sub-achievement-completed">${completedDateFormat}</div>
         <div class="sub-achievement-description">${description}</div>`;
       })
       .join('');
   }
+
+  renderSubAchievementsReview() {
+    return this.state.subAchievements
+      .filter(item => item.review)
+      .map(({ title, review, completed, completedDate, completedYear, isFailure }) => {
+        const icon = this.renderIcon({ isFailure, completed });
+        const completedDateFormat = completedDate ? completedDate : completedYear ? completedYear : '';
+
+        return `<div class="sub-achievement-title">
+          ${icon} ${title}</span>
+        </div>
+        <div class="sub-achievement-completed">${completedDateFormat}</div>
+        <div class="sub-achievement-description">${review}</div>`;
+      })
+      .join('');
+  }
+
   render() {
     const {
       id,
@@ -39,6 +57,7 @@ export default class AchievementsCard {
       description,
       completed,
       completedDate,
+      completedYear,
       review,
       images,
       isFailure,
@@ -46,14 +65,49 @@ export default class AchievementsCard {
     } = this.state;
 
     const icon = this.renderIcon({ isFailure, completed });
+    const completedDateFormat = completedDate ? completedDate : completedYear ? completedYear : '';
+    const subAchievementRate = (subAchievements.filter(item => item.completed).length / subAchievements.length) * 100;
+    const hasSubReview = subAchievements.some(item => item.review);
+    const front = `<div class="front">
+                    <dl>
+                      <dt class="achievement-title">${icon} ${title}</dt>
+                      <dd class="achievement-description">${description} 
+                        ${
+                          completedDateFormat &&
+                          `<span class="achievement-completed-date">${completedDateFormat}</span>`
+                        }
+                      </dd>
+                    </dl>
+                    ${
+                      subAchievements.length > 0
+                        ? `<div class="sub-achievements-list">
+                            <div class="progress-bar">
+                              <span class="box-progress" style="width: ${subAchievementRate}%"></span>
+                            </div>${this.renderSubAchievements()}
+                          </div>`
+                        : ''
+                    }
+                  </div>`;
+
+    const back = `<div class="back">
+                    <dl>
+                      <dt class="achievement-title">${title}</dt>
+                      <dd class="achievement-description">${review ? review : ''}
+                       ${
+                         completedDateFormat && `<span class="achievement-completed-date">${completedDateFormat}</span>`
+                       }</dd></dd>
+                      ${
+                        hasSubReview
+                          ? `<div class="sub-achievements-list">${this.renderSubAchievementsReview()}</div>`
+                          : ''
+                      }
+                    </dl>
+                  </div>`;
+
     this.target.innerHTML = `
-    <div class="front">
-      <dl>
-        <dt class="achievement-title">${icon} ${title}</dt>
-        <dd class="achievement-description">${description}</dd>
-      </dl>
-      ${subAchievements.length > 0 ? `<div class="sub-achievements-list">${this.renderSubAchievements()}</div>` : ''}
-    </div>`;
+    ${front}
+    ${review || hasSubReview ? back : ''}
+    `;
 
     this.container.appendChild(this.target);
   }
