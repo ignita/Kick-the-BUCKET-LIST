06620dd7531c57bba021c1f2119a569c1b95e51d
padosum <makalu171@gmail.com>
2023-01-10 21:03:44 +0900

    💄 Not Found Page 스타일 적용

--- src/client/views/NotFoundView.js
+++ src/client/views/NotFoundView.js
@@ -5,6 +5,11 @@ export default class NotFoundView {
   }
 
   render() {
-    this.container.innerHTML = '404 Not Found';
+    this.container.innerHTML = `
+    <div class="not-found">
+      <div class="not-found-box">
+        페이지를 찾을 수 없습니다.
+      </div>
+    </div>`;
   }
 }
